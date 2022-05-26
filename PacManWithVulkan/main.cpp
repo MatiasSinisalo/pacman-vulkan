2c2034d89d6abad68c4721d1c121da790aa8c46c
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-05-26 14:54:14 +0300

    Start of the project, Hello Window

--- PacManWithVulkan/main.cpp
+++ PacManWithVulkan/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 int main() {
 	std::cout << "hello world!";
+	if (!glfwInit())
+	{
+		// Initialization failed
+	}
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		
+		glfwPollEvents();
+	}
 	return 0;
 }
