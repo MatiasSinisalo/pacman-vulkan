8efa072a02111f8de5f30aad43b81b3eeadc0bd8
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-05-26 15:57:12 +0300

    Created the vulkan instance

--- PacManWithVulkan/main.cpp
+++ PacManWithVulkan/main.cpp
@@ -1,12 +1,60 @@
+#include <vulkan/vulkan.h>
+
+#include <stdio.h>
 #include <iostream>
 #include <GLFW/glfw3.h>
+
+/*
+Sources:
+huge credits go to the totorial from: https://vulkan-tutorial.com/
+GLFW vulkan guide: https://www.glfw.org/docs/3.3/vulkan_guide.html
+GLFW getiing started: https://www.glfw.org/docs/3.3/quick.html
+c Multiline makros: https://www.geeksforgeeks.org/multiline-macros-in-c/?ref=lbp
+
+*/
+
+#define CHECK_VK_ERROR(f) {\
+	VkResult result = f;\
+	if (result != VK_SUCCESS){ \
+		throw std::runtime_error("failed to create instance!"); \
+	}\
+}
+
 int main() {
+	
 	std::cout << "hello world!";
 	if (!glfwInit())
 	{
 		// Initialization failed
 	}
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+
+	VkInstance instance = {};
+	
+	VkApplicationInfo vulkanAppInfo = {};
+	vulkanAppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	vulkanAppInfo.pApplicationName = "PacManWithVulkan";
+	vulkanAppInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	vulkanAppInfo.pEngineName = "No Engine";
+	vulkanAppInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	vulkanAppInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+	VkInstanceCreateInfo instanceCreateInfo = {};
+	instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceCreateInfo.pNext = NULL;
+	instanceCreateInfo.flags = 0;
+	instanceCreateInfo.pApplicationInfo = &vulkanAppInfo;
+
+	uint32_t count;
+	const char** extensions = glfwGetRequiredInstanceExtensions(&count);
+	instanceCreateInfo.enabledExtensionCount = count;
+	instanceCreateInfo.ppEnabledExtensionNames = extensions;
+	instanceCreateInfo.enabledLayerCount = 0;
+
+	CHECK_VK_ERROR(vkCreateInstance(&instanceCreateInfo, nullptr, &instance));
 
 	while (!glfwWindowShouldClose(window))
 	{
