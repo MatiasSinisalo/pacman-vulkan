3aa8d338f43e558d2e777aa8ce516c6867948260
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-06-20 13:32:41 +0300

    started work on collisions

--- PacManWithVulkan/game.cpp
+++ PacManWithVulkan/game.cpp
@@ -7,20 +7,43 @@
 #include <GLFW/glfw3native.h>
 
 #include "globalRenderStructures.h"
-#define MAZEWIDTH 1
-#define MAZEHEIGHT 1
 
 
 class Game {
 public: 
 	std::vector<gameObject> allGameObjects;
-	
+	gameObject *player;
 	void createGameObjects() {
+
+		
+		for (int j = 0; j < 10; j++) {
+			for (int i = 0; i < 10; i++) {
+
+
+				if (i == 0 || j == 0 || i == 9 || j == 9) {
+					gameObject newGameObject = {};
+					newGameObject.pos = glm::vec3(-7.0f + 1.5 * i, -7.0f + 1.5 * j, 1.0f);
+					newGameObject.scale = glm::vec3(1.5f, 1.5f, 1.0f);
+
+					sprite newSprite = {};
+					newSprite.model = glm::scale(glm::mat4(1.0f), newGameObject.scale);
+					glm::vec3 newPos = newGameObject.pos;
+					newPos.x *= 1 / newGameObject.scale.x;
+					newPos.y *= 1 / newGameObject.scale.y;
+					newSprite.model = glm::translate(newSprite.model, newPos);
+					newSprite.textureIndex = 2;
+					newGameObject.drawObject = newSprite;
+					allGameObjects.push_back(newGameObject);
+				}
+			}
+		}
+	
+		
 		int textureIndex = 0;
 		for (int i = 0; i < 2; i++) {
 			gameObject newGameObject = {};
 			newGameObject.pos = glm::vec3(0.0f + i, 0.0f, 1.0f);
-			newGameObject.scale = glm::vec3(1.5f, 1.5f, 1.0f);
+			newGameObject.scale = glm::vec3(0.5f, 0.5f, 1.0f);
 
 			sprite newSprite = {};
 			newSprite.model = glm::scale(glm::mat4(1.0f), newGameObject.scale);
@@ -38,12 +61,16 @@ public:
 				textureIndex = 0;
 			}
 			newGameObject.drawObject = newSprite;
+			
 			allGameObjects.push_back(newGameObject);
+			
 		}
-	
+		player = &allGameObjects[allGameObjects.size() - 2];
+
+		
 	}
 	
-	void colliding(gameObject first, gameObject second) {
+	bool colliding(gameObject first, gameObject second) {
 		std::cout << first.pos.x << ", " << first.pos.y << "|";
 		std::cout << second.pos.x << ", " << second.pos.y;
 		if ((first.pos.x + 0.5f * first.scale.x) > (second.pos.x - 0.5f * second.scale.x) && (first.pos.x - 0.5f * first.scale.x) < (second.pos.x + 0.5f * second.scale.x) &&
@@ -51,41 +78,53 @@ public:
 
 			) {
 			std::cout << ", collision! ";
-
+			return true;
 		}
 		else {
 			//std::cout << "no collision!\n";
+			
 		}
 		std::cout << "\n";
+		return false;
 	}
 
 	void handleInput(GLFWwindow* window, std::vector<gameObject>& gameObjects, float ellapsed) {
 		float ellapsedInSeconds = (ellapsed / pow(10, 6));
 		int moveRight = glfwGetKey(window, GLFW_KEY_D);
-		glm::vec3 oldpos = gameObjects[0].pos;
+		glm::vec3 oldpos = player->pos;
 		if (moveRight != 0) {
-			gameObjects[0].pos.x += 10.0f * ellapsedInSeconds;
+			player->pos.x += 10.0f * ellapsedInSeconds;
 		}
 
 		int moveLeft = glfwGetKey(window, GLFW_KEY_A);
 		if (moveLeft != 0) {
-			gameObjects[0].pos.x -= 10.0f * ellapsedInSeconds;
+			player->pos.x -= 10.0f * ellapsedInSeconds;
 		}
 
 		int moveUp = glfwGetKey(window, GLFW_KEY_W);
 		if (moveUp != 0) {
-			gameObjects[0].pos.y -= 10.0f * ellapsedInSeconds;
+			player->pos.y -= 10.0f * ellapsedInSeconds;
 		}
 
 		int moveDown = glfwGetKey(window, GLFW_KEY_S);
 		if (moveDown != 0) {
-			gameObjects[0].pos.y += 10.0f * ellapsedInSeconds;
+			player->pos.y += 10.0f * ellapsedInSeconds;
 		}
-		glm::vec3 newPos = -(oldpos - gameObjects[0].pos);
-		newPos.x *= 1 / gameObjects[0].scale.x;
-		newPos.y *= 1 / gameObjects[0].scale.y;
-		gameObjects[0].drawObject.model = glm::translate(gameObjects[0].drawObject.model, newPos);
-		colliding(gameObjects[0], gameObjects[1]);
+
+		for (gameObject &object : gameObjects) {
+			if (player != &object) {
+				if (colliding(*player, object)) {
+					player->pos = oldpos;
+
+				}
+			}			
+		}
+
+		glm::vec3 newPos = -(oldpos - player->pos);
+		newPos.x *= 1 / player->scale.x;
+		newPos.y *= 1 / player->scale.y;
+		player->drawObject.model = glm::translate(player->drawObject.model, newPos);
+		
 	}
 };
 
